0b09f4132d1eb7be7999f3559ae6f807612b2dc2
Erickson Elías <erick.yasir17@gmail.com>
2018-05-28 23:33:54 -0600

    Archivo conexión
    se establece la conexión a la base de  datos Personal

---
+++ conexion.php
@@ -0,0 +1,11 @@
+<?php  
+
+$mysqli = new mysqli('localhost', 'root','','personal');
+
+if($mysqli->connect_error){
+    die('Error en la conexion' . $mysqli->connect_error);
+}
+
+printf("Servidor Informacion: %s\n", $mysqli->server_info);
+
+?>
